c692628d953a5076874ab08d24e9238cf8169b48
Beste Uncu <besteuncu@gmail.com>
2025-07-30 20:02:47 +0300

    .

---
+++ R/global_variables.R
@@ -0,0 +1,6 @@
+
+utils::globalVariables(c("Full_Name", "Silhouette", "CH", "DB", "Dunn", 
+                         "Value", "Algorithm", "Variant", "Type", 
+                         "Silhouette_norm", "CH_norm", "DB_norm", 
+                         "Dunn_norm", "Metric", "Normalized_Value", 
+                         "Overall_Score", "."))
